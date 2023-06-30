cd7804620a3317e8330fccb3ea4ce380fbbec4f6
Gustavo Denobi <106932599+DenobiGustavo@users.noreply.github.com>
2023-06-30 20:43:24 -0300

    Update HospitalVeterinario.c

--- programming_c/work2/HospitalVeterinario.c
+++ programming_c/work2/HospitalVeterinario.c
@@ -1,68 +1,349 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct Register
+struct 
 {
-    char nameofanimal[16];
-    char nameofowner[16];
-    char species[16];
+    char nameofanimal[30];
+    char nameofowner[30];
+    char species[30];
+    char symptom[30];
+    char diagnostic[30];
     int age;
-    char symptom[20];
-    char diagnostic[40];
-};
+}pet;
 
 int main(){
 
-struct Register pet;
-int option = 0;
+int option_access = 0;
+int option_menu = 0;
+FILE *file;
 
-printf("===================== MENU ====================\n");
-printf("----> (1)Cadastrar\n----> (2)Consultar\n----> (3)Alterar\n----> (4)Remover\n----> (5)Exibir\n");
-printf("Digite o a opcao desejada: \n");
+printf("  \n");
+printf("========================+++++++++++++++++++++++========================\n");
+printf("=======================+++ !!! BEM VINDO !!! +++=======================\n");
+printf("========================+++++++++++++++++++++++========================\n");
+printf("  \n");
 
-scanf("%d", &option);
+printf("------------> Deseja acessar o menu? (1)Sim (-1)Nao: \n");
+scanf("%d", &option_access);
 
-if(option == 1){
+system("cls");
+    if(option_access == -1){
 
-    FILE *arq;
-    arq = fopen("cadastrar.txt", "w");
+        printf("Ate mais!!!\n");
+        exit(1);
 
-    if(arq == NULL){
+    }
+
+    else if(option_access == 1){
+        
+        do{
+
+            printf("========================+++++++++++++++++++++++========================\n");
+            printf("======================++++++ !!! MENU !!! +++++++======================\n");
+            printf("========================+++++++++++++++++++++++========================\n");
+            printf("  \n");
+
+            printf("----> (1)Cadastrar\n----> (2)Consultar\n----> (3)Alterar\n----> (4)Remover\n----> (5)Exibir\n----> (6)Sair\n");
+            printf("  \n");
+
+            printf("------------> Digite o a opcao desejada: ");
+            scanf("%d", &option_menu);
+
+        
+            system("cls");
+
+                switch(option_menu){
+                
+                case 1:
+                    register_pet();
+                    break;
+                
+                case 2:
+                    consult_pet();
+                    break;
+
+                case 3:
+                    change_pet();
+                    break;
+
+                case 4:
+                    remove_pet();
+                    break;
+
+                case 5:
+                    display_pet();
+                    break;
+
+                case 6:
+
+                    printf("========================+++++++++++++++++++++++========================\n");
+                    printf("====================++++++ !!! ATE MAIS !!! +++++++====================\n");
+                    printf("========================+++++++++++++++++++++++========================\n");
+                    printf("  \n");
+                    break;
+
+                default:
+                    printf("Voce digitou uma opcao invalida...\n");
+                    printf("  \n");
+                    break;
+
+                }
+
+            }while(option_menu != 6); 
+        }
+}
+
+int register_pet(){
+
+FILE *file;
+char codigo[100];
+char nameregister[100];
+
+printf("============================ AREA DE REGISTRO ============================\n");
+printf("  \n");
+
+printf("------------>Digite o codigo de seu animal: ");
+scanf("%s", &codigo);
+sprintf(nameregister,"%s.txt", codigo);    
+
+system("cls");
+
+file = fopen(nameregister, "w");
+
+    if(file == NULL){
 
         printf("Erro ao cadastrar...\n");
-        exit(1);
+        printf(" \n");
+        return 1;
+                        
+    }
+
+setbuf(stdin, 0);
+printf("Digite o nome de seu pet: ");
+fgets(pet.nameofanimal, sizeof(pet.nameofanimal), stdin);
+
+printf("Digite o nome do dono: ");
+fgets(pet.nameofowner, sizeof(pet.nameofowner), stdin);
+
+printf("Digite a especie do seu pet: ");
+fgets(pet.species, sizeof(pet.species), stdin);
+
+printf("Digite os sintomas do seu pet: ");
+fgets(pet.symptom, sizeof(pet.symptom), stdin);
+
+printf("Digite o diagnostico do seu pet: ");
+fgets(pet.diagnostic, sizeof(pet.diagnostic), stdin);
+
+printf("Digite a idade do seu pet: ");
+scanf("%d", &pet.age);
+
+int result = fprintf(file, "Nome:%sDono:%sEspecie:%sSintomas:%sDiagnostico:%sIdade:%d\n", pet.nameofanimal, pet.nameofowner, pet.species, pet.symptom, pet.diagnostic, pet.age);
+    
+    if(result < 0){
+
+        printf("Erro ao cadastrar...\n");
+        printf(" \n");
+
+    }
+
+fclose(file);
+
+file = fopen("Cadastro.txt", "a");
+
+    if(file == NULL){
+
+        printf("Erro ao cadastrar...\n");
+        printf(" \n");
+        return 1;
+                
+    }
+
+fprintf(file, "Cadastro%s\n", nameregister);
+
+fclose(file);
+
+system("cls");
+return 0;
+
+}
+
+int consult_pet(){
+
+FILE *file;
+char codigo[100];
+char nameregister[100];
+
+printf("============================ AREA DE CONSULTA ============================\n");
+printf("  \n");
+
+printf("------------> Digite qual registro voce quer acessar: ");
+printf("  \n");
+scanf("%s", &codigo);
+
+sprintf(nameregister,"%s.txt", codigo);
+
+system("cls");
+
+file = fopen(nameregister, "r");
+
+    if(file == NULL){
+
+        printf("Erro ao consultar...\n");
+        printf(" \n");
+        return(1);
         
     }
 
-    setbuf(arq, 0);
+char r;
 
-    printf("Digite o nome de seu pet: ");
-    scanf("Nome: %s", pet.nameofanimal);
-        fprintf(arq,"%s\n", pet.nameofanimal);
+while ((r = fgetc(file)) != EOF) {
 
-    printf("Digite o nome do dono: ");
-    scanf("Dono: %s", pet.nameofowner);
-        fprintf(arq,"%s\n", pet.nameofowner);
+    printf("%c", r);
+}
+printf(" \n");
 
-    printf("Digite a especie do seu pet: ");
-    scanf("Especie: %s", pet.species);
-        fprintf(arq,"%s\n", pet.species);
-       
-    printf("Digite a idade do seu pet: ");
-    scanf("Idade: %d", &pet.age);
-        fprintf(arq,"%d\n", pet.age);
+fclose(file);
 
-    printf("Digite os sintomas do seu pet: ");
-    scanf("Sintomas: %s", pet.symptom);
-        fprintf(arq,"%s\n", pet.symptom);
-
-    printf("Digite o diagnostico do seu pet: ");
-    scanf("Diagnostico: %s", pet.diagnostic);
-        fprintf(arq,"%s\n", pet.diagnostic);
- 
-    fclose(arq);
-       
-    }
+return 0;
 
 }
+
+int change_pet(){
+
+FILE *file;
+char codigo[100];
+char nameregister[100];
+
+printf("============================ AREA DE ALTERACAO ============================\n");
+printf("  \n");
+
+printf("------------> Digite o codigo que deseja alterar: ");
+scanf("%s", &codigo);
+sprintf(nameregister,"%s.txt", codigo);
+
+system("cls");
+
+int verification = remove(nameregister);
+
+    if(verification != 0){
+
+        printf("Erro ao alterar o arquivo...\n");
+        printf(" \n");
+        return 1;
+        
+    }
+            
+file = fopen(nameregister, "w");
+
+    if(file == NULL){
+
+        printf("Erro ao cadastrar novo arquivo...\n");
+        printf(" \n");
+        return 1;
+                        
+    }
+
+setbuf(stdin, 0);
+printf("Digite o nome de seu pet: ");
+fgets(pet.nameofanimal, sizeof(pet.nameofanimal), stdin);
+
+printf("Digite o nome do dono: ");
+fgets(pet.nameofowner, sizeof(pet.nameofowner), stdin);
+
+printf("Digite a especie do seu pet: ");
+fgets(pet.species, sizeof(pet.species), stdin);
+
+printf("Digite os sintomas do seu pet: ");
+fgets(pet.symptom, sizeof(pet.symptom), stdin);
+
+printf("Digite o diagnostico do seu pet: ");
+fgets(pet.diagnostic, sizeof(pet.diagnostic), stdin);
+
+printf("Digite a idade do seu pet: ");
+scanf("%d", &pet.age);
+
+int result = fprintf(file, "Nome:%sDono:%sEspecie:%sSintomas:%sDiagnostico:%sIdade:%d\n", pet.nameofanimal, pet.nameofowner, pet.species, pet.symptom, pet.diagnostic, pet.age);
+    
+    if(result < 0){
+
+        printf("Erro ao cadastrar novo arquivo...\n");
+        printf(" \n");
+
+        return 1;
+        
+    }
+
+fclose(file);
+
+system("cls");
+return 0;
+
+}
+
+int remove_pet(){
+
+char codigo[100];
+char nameregister[100];
+char nameregister2[100];
+
+printf("============================ AREA DE REMOCAO ============================\n");
+printf("  \n");
+
+printf("------------> Digite o codigo que deseja remover: ");
+scanf("%s", &codigo);
+sprintf(nameregister,"%s.txt", codigo);
+
+system("cls");
+
+int verification = remove(nameregister);
+
+    if(verification != 0){
+
+        printf("Erro ao remover o arquivo...\n");
+        printf(" \n");
+
+        return 1;
+
+    }
+
+    system("cls");
+    return 0;
+
+}
+
+int display_pet(){
+
+FILE *file;
+
+printf("============================ AREA DE EXIBICAO ============================\n");
+printf("  \n");
+
+file = fopen("Cadastro.txt", "r");
+
+    if(file == NULL){
+
+        printf("Erro a exibir arquivo...\n");
+        printf(" \n");
+        return 1;
+                        
+    }
+
+char r;
+    while ((r = fgetc(file)) != EOF) {
+
+        printf("%c", r);
+    }
+
+    printf(" \n");
+    
+    fclose(file);
+
+    return 0;
+
+}
+    
+
+    
+
+    
